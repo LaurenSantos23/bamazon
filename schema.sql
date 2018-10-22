0f920b69f811a8bfd93f563e6b7eb96b73c6f7df
LaurenSantos23 <Lauren.Santosuosso@yahoo.com>
2018-10-22 13:12:48 -0400

    added values for rows

--- schema.sql
+++ schema.sql
@@ -1,20 +1,28 @@
-drop database if exists bamazon;
+DROP DATABASE IF EXISTS bamazon;
 
-create database bamazon;
+CREATE DATABASE bamazon;
 
-use bamazon;
+USE bamazon;
 
-create table products (
-    item_id int auto_increment not null,
-    product_name varchar(100) not null,
-    department_name VARCHAR(100) not null,
-    price DECIMAL(10,2) not null,
-    stock_quantity INT(10) not null,
-    primary key (item_id)
+CREATE TABLE products (
+    item_id INT auto_increment NOT NULL,
+    product_name VARCHAR(100) NOT NULL,
+    department_name VARCHAR(100) NO NULL,
+    price DECIMAL(10,2) NOT NULL,
+    stock_quantity INT(10) NOT null,
+    PRIMARY KEY (item_id)
     );
 
 SELECT * FROM products;
 
 INSERT INTO products(product_name,department_name,price,stock_quantity)
-VALUES ("Monopoly","Games",10.00, 5),
-    ("Guess Who", "Games", 15.00, 10);
+VALUES ("Monstera Plant","Garden",75.00, 25),
+    ("20 Year Juniper Bonsai Tree", "Garden", 525.00, 30),
+    ("60 Year Japanese Maple Bonsai Tree", "Garden", 1000.00, 5),
+    ("Moroccan Wedding Blanket","Home Decor", 356.00, 25),
+    ("Vintage Chinese Wedding Bed","Furniture",5,500.00, 12),
+    ("Moroccan Rug","Home Decor", 150.00, 20),
+    ("Vintage Banjera Toran", "Home Decor", 250.00, 10),
+    ("Antique French Wardrobe", "Furniture", 750.00, 5 ),
+    ("Antique Victorian Silver Candle Holder (2 in set)", "Home Decor", 350.00, 20),
+    ("Vintage Silk Screen Japanese Room Divider", "Home Decor", 1500.00, 5);
