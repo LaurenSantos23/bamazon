6f3bba6ae7f0463c99148e26724c573efcb529b9
LaurenSantos23 <Lauren.Santosuosso@yahoo.com>
2018-10-22 13:55:40 -0400

    removed extra comma  from line 23

--- schema.sql
+++ schema.sql
@@ -7,7 +7,7 @@ USE bamazon;
 CREATE TABLE products (
     item_id INT auto_increment NOT NULL,
     product_name VARCHAR(100) NOT NULL,
-    department_name VARCHAR(100) NO NULL,
+    department_name VARCHAR(100) NOT NULL,
     price DECIMAL(10,2) NOT NULL,
     stock_quantity INT(10) NOT null,
     PRIMARY KEY (item_id)
@@ -15,12 +15,12 @@ CREATE TABLE products (
 
 SELECT * FROM products;
 
-INSERT INTO products(product_name,department_name,price,stock_quantity)
+INSERT INTO products (product_name,department_name,price,stock_quantity)
 VALUES ("Monstera Plant","Garden",75.00, 25),
     ("20 Year Juniper Bonsai Tree", "Garden", 525.00, 30),
     ("60 Year Japanese Maple Bonsai Tree", "Garden", 1000.00, 5),
     ("Moroccan Wedding Blanket","Home Decor", 356.00, 25),
-    ("Vintage Chinese Wedding Bed","Furniture",5,500.00, 12),
+    ("Vintage Chinese Wedding Bed","Furniture",5500.00, 12),
     ("Moroccan Rug","Home Decor", 150.00, 20),
     ("Vintage Banjera Toran", "Home Decor", 250.00, 10),
     ("Antique French Wardrobe", "Furniture", 750.00, 5 ),
